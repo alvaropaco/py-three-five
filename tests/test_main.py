41be7a7fa47ab8644e3e1f2ff8208d2e5c97287e
Alvaro Paco <alvaropaconeto@gmail.com>
2019-03-31 18:00:35 -0300

    Validation PEP8 rules on test files @alvaropaco

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,12 +4,12 @@ import pytest
 
 TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), 'output.txt')
 
-def test_init():
-  from .. import module
-  output = open(TESTDATA_FILENAME, 'r').read()
-  with mock.patch.object(module, "main", return_value=output):
-    with mock.patch.object(module, "__name__", "__main__"):
-      with mock.patch.object(module.sys,'exit') as mock_exit:
-         module.init()
 
-         assert mock_exit.call_args[0][0] == output
+def test_init():
+    from .. import module
+    output = open(TESTDATA_FILENAME, 'r').read()
+    with mock.patch.object(module, "main", return_value=output):
+        with mock.patch.object(module, "__name__", "__main__"):
+            with mock.patch.object(module.sys, 'exit') as mock_exit:
+                module.init()
+                assert mock_exit.call_args[0][0] == output
